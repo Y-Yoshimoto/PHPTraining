8ba51d60ce34d783ef54789e03a05e68fe6cce80
Y-Yoshimoto <mewyoshimoto@gmail.com>
2019-09-10 03:57:29 +0900

    引数変更

--- ApatchPHP_Docker/apatchphp_custom/contents/bmi/Ajax/returnBMI.php
+++ ApatchPHP_Docker/apatchphp_custom/contents/bmi/Ajax/returnBMI.php
@@ -4,38 +4,37 @@
     $tall = filter_input(INPUT_POST, 'tall');
     $weight = filter_input(INPUT_POST, 'weight');
     //BMI計算
-    $BMIresult = bmiCheck(bmiCal($tall, $weight));
-    //レスポンス生成
-    $result = array("bmi" => $BMIresult['bmi'],
-    "color" => $BMIresult["color"] ,
-    "text" => $BMIresult["text"],
-    "status" => $BMIresult['status']);
+    $BMIresult = bmiCheck($tall, $weight);
     //jsonオブジェクトエンコード,送信
-    echo json_encode($result);
+    echo json_encode($BMIresult);
     //echo var_dump($_POST);
     exit;
 ?>
 
 <?php
+    //BMI計算
     function bmiCal($tall, $weight){
             $tallm = $tall*0.01;
             return $weight/($tallm*$tallm);
     }
-    
-    function bmiCheck($bmi){
+    //BMI評価
+    function bmiCheck($tall, $weight){
+    //BMI計算
+    $bmi = bmiCal($tall, $weight);
     //BIM情報読み込み,デコード
     $BMIdata = json_decode(file_get_contents("../BMIdata.json"), true);
     foreach ($BMIdata["Info"] as &$Info) {
-        //echo 'bmiUpperLimit',$Info["bmiUpperLimit"],'<br>';
+        //BMI判定, レスポンス生成
         if($bmi < $Info["bmiUpperLimit"]){
             return makeInfo($bmi, $Info["color"], $Info["text"], $Info["status"]);
         };
     };
     return makeInfo($bmi, "#FFFFFF", "異常値です", "異常値");
     }
-
+    //レスポンス用JSON生成
     function makeInfo($bmi, $color, $text, $status){
-        $result = array("bmi" => $bmi,
+        $result = array(
+        "bmi" => $bmi,
         "color" => $color,
         "text" => $text,
         "status" => $status) ;
